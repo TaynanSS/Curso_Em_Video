736b136d7c70c3f2b0d43927c7e41268d2caa83d
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-07 11:03:35 -0300

    Update ex024.py

--- CursoemVideoExercicios/ex024.py
+++ CursoemVideoExercicios/ex024.py
@@ -2,6 +2,9 @@
 
 nome = str(input('Digite um nome de uma cidade: ')).strip().split()
 
-#comeca = nome.split()
+print(nome[0].lower() == 'santo') # Joga a entrada do usuário pra minúscula e verifica se é igual a "santo"
 
-print(nome[0].lower() == 'santo')
+#OUTRA FORMA DE FAZER
+
+nome = str(input('Digite um nome de uma cidade: ')
+print(cid[:5].upper{} == 'SANTO') # Joga tudo que foi digitado na entrada para maiúscula e verifica se é igual a "SANTO"     
