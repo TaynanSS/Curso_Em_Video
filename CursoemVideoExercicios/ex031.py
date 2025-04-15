4d9e7babd5fff47fc0550f2f186e44afe159e0fa
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-15 14:27:11 -0300

    Add files via upload

--- CursoemVideoExercicios/ex031.py
+++ CursoemVideoExercicios/ex031.py
@@ -2,15 +2,22 @@
 # Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km
 # e R$ 0,45 para viagens mais longas.
 
-km = float(input('Qual a distância da sua viagem em Km: '))
-
-cobrar1 = km * 0.5
-cobrar2 = km * 0.45
+km = float(input('Informe a distância da sua viagem em Km: '))
 
 if km < 0:
     print('Valor inválido. Digite um valor acima de 0Km')
 else:
     if km <= 200:
-        print('Sua viagem de {}Km custará {:.2f}'.format(km, cobrar1))
+        cobrar = km * 0.5
+        print('Sua viagem de {}Km custará R${:.2f}'.format(km, cobrar))
     else:
-        print('Sua viagem de {}Km custará {:.2f}'.format(km, cobrar2))
+        cobrar = km * 0.45
+        print('Sua viagem de {}Km custará R${:.2f}'.format(km, cobrar))
+
+# OUTRA FORMA DE FAZER
+
+km = float(input('Informe a distância da sua viagem em Km: '))
+
+preço = km * 0.50 if km <= 200 else km * 0.45
+
+print(f'O preço de sua passagem será de R${preço:.2f}')
