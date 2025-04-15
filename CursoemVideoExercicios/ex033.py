4de436fb58cd4943379b7ec115c3109150f3318b
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-15 08:43:24 -0300

    Add files via upload

--- CursoemVideoExercicios/ex033.py
+++ CursoemVideoExercicios/ex033.py
@@ -6,30 +6,60 @@ n3 = float(input('Digite o terceiro número: '))
 
 if n1 == n2 and n1 > n3:
     print('O primeiro e segundo número são iguais e os maiores.\n'
-          'Número digitado: {}'.format(n1))
+          f'Número digitado: {n1}')
 else:
     if n1 == n3 and n1 > n2:
         print('O primeiro e terceiro número são iguais e os maiores.\n'
-              'Número digitado: {}'.format(n1))
+              f'Número digitado: {n1}')
     else:
         if n1 > n2 and n1 > n3:
             print('O primeiro número é o maior.\n'
-                  'Número digitado: {}'.format(n1))
+                  f'Número digitado: {n1}')
         else:
             if n2 == n1 and n2 > n3:
                 print('O primeiro e segundo número são iguais e os maiores\n'
-                      'Número digitado: {}'.format(n2))
+                      f'Número digitado: {n2}')
             else:
                 if n2 == n3 and n2 > n1:
                     print('O segundo e terceiro número são iguais e os maiores.\n'
-                          'Número digitado: {}'.format(n2))
+                          f'Número digitado: {n2}')
                 else:
                     if n2 > n1 and n2 > n3:
                         print('O segundo número é o maior.\n'
-                              'Número digitado: {}'.format(n2))
+                              f'Número digitado: {n2}')
                     else:
                         if n1 == n2 and n1 == n3 and n2 == n3:
                             print('Todos os números são iguais e não há um maior.')
                         else:
                             print('O terceiro número é o maior.\n'
-                                  'Número digitado: {}'.format(n3))
+                                  f'Número digitado: {n3}')
+
+if n1 == n2 and n1 < n3:
+    print('O primeiro e segundo número são iguais e os menores.\n'
+          f'Número digitado: {n1}')
+else:
+    if n1 == n3 and n1 < n2:
+        print('O primeiro e terceiro número são iguais e os menores.\n'
+              f'Número digitado: {n1}')
+    else:
+        if n1 < n2 and n1 < n3:
+            print('O primeiro número é o menor.\n'
+                  f'Número digitado: {n1}')
+        else:
+            if n2 == n1 and n2 < n3:
+                print('O primeiro e segundo número são iguais e os menores\n'
+                      f'Número digitado: {n2}')
+            else:
+                if n2 == n3 and n2 < n1:
+                    print('O segundo e terceiro número são iguais e os menores.\n'
+                          f'Número digitado: {n2}')
+                else:
+                    if n2 < n1 and n2 < n3:
+                        print('O segundo número é o menor.\n'
+                              f'Número digitado: {n2}')
+                    else:
+                        if n1 == n2 and n1 == n3 and n2 == n3:
+                            print('Todos os números são iguais e não há um menor.')
+                        else:
+                            print('O terceiro número é o menor.\n'
+                                  f'Número digitado: {n3}')
