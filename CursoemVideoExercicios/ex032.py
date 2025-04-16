f8699a3d8ab69f3be828be58328587d86c83e5df
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-16 10:02:37 -0300

    Add files via upload

--- CursoemVideoExercicios/ex032.py
+++ CursoemVideoExercicios/ex032.py
@@ -1,14 +1,13 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
 
+from datetime import date
+
 ano = int(input('Digite um ano: '))
 print('Caso queira analisar o ano atual, digite 0.')
 
 anobi = ano % 4
 anobi1 = ano % 400
 naobi = ano % 100
-
-if ano == 0:
-    print()
 
 if anobi == 0 and anobi1 == 0 and naobi == 0:
         print('O ano {} é bissexto'.format(ano))
@@ -17,4 +16,17 @@ else:
         print('O ano {} é bissexto.'.format(ano))
     else:
         print('O ano {} não é bissexto.'.format(ano))
+
+# OUTRA FORMA DE FAZER
+
+ano = int(input('Digite um ano: '))
+print('Caso queira analisar o ano atual, digite 0.')
+
+if ano == 0:
+    ano = date.today().year
+
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é BISSEXTO.')
+else:
+    print(f'O ano {ano} NÃO é BISSEXTO.')
 
