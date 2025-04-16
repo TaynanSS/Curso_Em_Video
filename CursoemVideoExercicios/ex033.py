563a1bf04abeaffe847d996c699ab70a30b82671
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-16 10:25:13 -0300

    Add files via upload

--- CursoemVideoExercicios/ex033.py
+++ CursoemVideoExercicios/ex033.py
@@ -4,6 +4,7 @@ n1 = float(input('Digite o primeiro número: '))
 n2 = float(input('Digite o segundo número: '))
 n3 = float(input('Digite o terceiro número: '))
 
+# Abaixo irá verificar quem é o maior
 if n1 == n2 and n1 > n3:
     print('O primeiro e segundo número são iguais e os maiores.\n'
           f'Número digitado: {n1}')
@@ -34,6 +35,7 @@ else:
                             print('O terceiro número é o maior.\n'
                                   f'Número digitado: {n3}')
 
+# Abaixo irá verificar quem é o menor
 if n1 == n2 and n1 < n3:
     print('O primeiro e segundo número são iguais e os menores.\n'
           f'Número digitado: {n1}')
@@ -63,3 +65,26 @@ else:
                         else:
                             print('O terceiro número é o menor.\n'
                                   f'Número digitado: {n3}')
+
+# OUTRA FORMA DE FAZER
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+
+# Verificando quem é o menor
+menor = a # Aqui suponhamos que o primeiro valor já é o menor
+if b<a and b<c:
+    menor = b
+if c<a and c < b:
+    menor = c
+
+# Verificando quem é o maior
+maior = a # Aqui suponhamos que o primeiro valor já é o maior
+if b>a and b>c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
