bd2e5c0fffc09734132830801d6596fcd67ab066
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-24 12:38:06 -0300

    Add files via upload

---
+++ CursoemVideoExercicios/ex042.py
@@ -0,0 +1,31 @@
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+
+# - Equilátero: Todos os lados iguais
+# - Isósceles: Dois lados iguais
+# - Escaleno: Todos os lados diferentes
+
+# DESAFIO 035: # Desenvolva um programa que leia o comprimento de três retas
+               # e diga ao usuário se elas podem ou não formar um triângulo.
+
+r1 = float(input('Comprimento da primeira reta: '))
+r2 = float(input('Comprimento da segunda reta: '))
+r3 = float(input('Comprimento da terceira reta: '))
+
+if r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1 and r1 == r2 and r1 == r3:
+    print('Todos os lados são iguais.\nO triângulo é \033[1mEquilátero.\033[m\n')
+    print(f'As retas de comprimento {r1:.2f}, {r2:.2f} e {r3:.2f} formam um triângulo.')
+elif r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1 and r1 == r2 and r1 != r3 or r1 == r3 and r1 != r2 or r2 == r3 and r2 != r1:
+    print('Dois lados são iguais.\nO triângulo é \033[1;mIsósceles.\033[m')
+elif True and r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
+    print('Todos os lados são diferentes.\nO triângulo é \033[1mEscaleno.\033[m')
+    print(f'As retas de comprimento {r1:.2f}, {r2:.2f} e {r3:.2f} formam um triângulo.')
+else:
+    print(f'As retas de comprimeiro {r1:.2f}, {r2:.2f} e {r3:.2f} não formam um triângulo.')
+
+
+
+
+
+    
+
+    
