f9b4f705cc41d787b9d3b2716e901eea5ef085a5
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-04-28 09:39:31 -0300

    Update ex042.py

--- CursoemVideoExercicios/ex042.py
+++ CursoemVideoExercicios/ex042.py
@@ -21,11 +21,21 @@ elif True and r1 + r2 > r3 and r1 + r3 > r2 and r2 + r3 > r1:
     print(f'As retas de comprimento {r1:.2f}, {r2:.2f} e {r3:.2f} formam um triângulo.')
 else:
     print(f'As retas de comprimeiro {r1:.2f}, {r2:.2f} e {r3:.2f} não formam um triângulo.')
+  
 
+# OUTRA FORMA DE FAZER
 
+r1 = float(input('Comprimento da primeira reta: '))
+r2 = float(input('Comprimento da segunda reta: '))
+r3 = float(input('Comprimento da terceira reta: '))
 
-
-
-    
-
-    
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print(f'As retas de comprimeiro {r1:.2f}, {r2:.2f} e {r3:.2f} formam um triângulo.')
+  if r1 == r2 == r3:
+      print('Todos os lados são iguais.\nO triângulo é \033[1mEquilátero.\033[m\n')
+  elif r1 != r2 != r3 != r1:
+      print('Todos os lados são diferentes.\nO triângulo é \033[1mEscaleno.\033[m')
+  else:
+      print('Dois lados são iguais.\nO triângulo é \033[1;mIsósceles.\033[m')
+else: 
+    print(f'As retas de comprimeiro {r1:.2f}, {r2:.2f} e {r3:.2f} não formam um triângulo.')
