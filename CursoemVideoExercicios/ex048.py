c4dd0108ec07021518cb4cbb64da474b5696849f
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-05-02 11:28:56 -0300

    Delete CursoemVideoExercicios/ex048.py

--- CursoemVideoExercicios/ex048.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três e
-# que se encontram no intervalo de 1 até 500
-
-somatorio = 0
-for c in range(1, 500, 2):
-    if c % 3 == 0:
-        pass
-    somatorio += c
-print(f'Somatório de todos os números ímpares: {somatorio}')
