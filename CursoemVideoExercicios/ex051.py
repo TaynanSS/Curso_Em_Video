75bd1d6b14af7aceb9fd1a99fad16f18d72d8993
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-05-02 13:37:12 -0300

    Delete CursoemVideoExercicios/ex051.py

--- CursoemVideoExercicios/ex051.py
+++
@@ -1,9 +0,0 @@
-# Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
-# No final, mostre os 10 primeiros termos dessa progressão.
-
-primeirot = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão: '))
-
-for c in range(10):
-    termo = primeirot + c * razao
-    print(termo)
