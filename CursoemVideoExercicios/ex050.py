71237e7c613a00b4214271c4c1b5d2086dc5bd18
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-05-02 13:37:21 -0300

    Delete CursoemVideoExercicios/ex050.py

--- CursoemVideoExercicios/ex050.py
+++
@@ -1,9 +0,0 @@
-# Desenvolva um programa que leia seis número inteiros e
-# moster a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
-
-soma = 0
-for c in range(0, 6):
-    num = int(input('Digite um número: '))
-    if num % 2 == 0:
-        soma += num
-print(f'O somatório dos números pares digitados: {soma}')
