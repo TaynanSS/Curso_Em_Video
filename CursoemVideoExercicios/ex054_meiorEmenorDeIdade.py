6a563abc0319647c5353055b6aeb7d70c3d8e71b
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-05-21 15:07:12 -0300

    Update ex054_meiorEmenorDeIdade.py

--- CursoemVideoExercicios/ex054_meiorEmenorDeIdade.py
+++ CursoemVideoExercicios/ex054_meiorEmenorDeIdade.py
@@ -9,10 +9,10 @@ menor = 0
 for c in range(0, 7):
     ano = int(input('Digite seu ano de nascimento: '))
     idade = atual - ano
-    if idade < 21:
-        menor += 1
-    elif idade >= 21:
+    if idade >= 21:
         maior += 1
+    else:
+        menor += 1
 if maior == 0:
     print('\n' + f'Há {menor} menores de idade.')
     print('Não há nenhum maior de idade!')
