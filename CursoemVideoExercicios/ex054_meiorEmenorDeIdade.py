dd7177929c0f349c4a23ccabcc5ea9df12941447
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-05-21 14:57:27 -0300

    Update ex054_meiorEmenorDeIdade.py

--- CursoemVideoExercicios/ex054_meiorEmenorDeIdade.py
+++ CursoemVideoExercicios/ex054_meiorEmenorDeIdade.py
@@ -1,11 +1,14 @@
 # Crie um programa que leia o ano de nascimento de sete pessoas.
 # No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 
+from datetime import date
+atual = date.today().year
 maior = 0
 menor = 0
+
 for c in range(0, 7):
     ano = int(input('Digite seu ano de nascimento: '))
-    idade = 2025 - ano
+    idade = atual - ano
     if idade < 21:
         menor += 1
     elif idade >= 21:
