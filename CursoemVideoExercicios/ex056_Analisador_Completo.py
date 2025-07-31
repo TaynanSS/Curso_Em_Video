3586ef77ecfbfed516edc60cbd77c53c17355ee5
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-07-31 18:56:25 -0300

    Update ex056_Analisador_Completo.py

--- CursoemVideoExercicios/ex056_Analisador_Completo.py
+++ CursoemVideoExercicios/ex056_Analisador_Completo.py
@@ -8,19 +8,21 @@ idadevelho = 0
 contM = 0
 media = 0
 for i in range(1,5):
-    print('-'*5, f'{i}° PESSOA', '-'*5)
+    print('-'*5, f'{i}° PESSOA', '-'*5)        # Pra deixar mais organizado
     nome = str(input('Digite seu nome: ')).strip()
     idade = int(input('Informe sua idade: '))
     sexo = str(input('Gênero (F/M): ')).upper().strip()
 
-    if sexo == 'F':
-        if idade < 20:
-            contM += 1
+    if sexo == 'F':    # Aqui já farei o filtro em mulheres abaixo de 20anos e coloco um contador sempre que houver outra.
+        if idade < 20:    
+            contM += 1    
 
-    if sexo == 'M' and idade > idadevelho:
-        idadevelho = idade
-        maisvelho = nome
+    if sexo == 'M' and idade > idadevelho:    # Na primeira repetição irá substituir o valor de "idadevelho" para o que entrou em "idade"
+        idadevelho = idade        # caso entre outro valor acima do que está em "idadevelho", ele é substituído novamente.
+        maisvelho = nome # Aqui salvará o último nome que passou por esse filtro.
 
-    media += idade
+    media += idade    # somatório básico
 
-print(f'Idade média do grupo: {media/4}\n', f'Homem mais velho: {maisvelho.capitalize()} - Idade: {idadevelho}\n', f'Mulheres com menos de 20anos: {contM}')
+print(f'Idade média do grupo: {media/4}\n',    # printa já fazendo a divisão por 4, economizando uma linha
+      f'Homem mais velho: {maisvelho.capitalize()} - Idade: {idadevelho}\n',    # Coloquei um Capitalize para sempre no primeiro nome vim a 1°letra em maiúsculo. Apenas algo visual.
+      f'Mulheres com menos de 20anos: {contM}')    
