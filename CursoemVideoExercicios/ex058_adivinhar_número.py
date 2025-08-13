a926cc72406fdfe47a81fe74a658475c625e4d98
Taynan Silva <47600771+TaynanSS@users.noreply.github.com>
2025-08-13 13:27:03 -0300

    Update ex058_adivinhar_número.py

--- CursoemVideoExercicios/ex058_adivinhar_número.py
+++ CursoemVideoExercicios/ex058_adivinhar_número.py
@@ -34,3 +34,24 @@ while escolha != computador:
     cont += 1
 
 print(f'Você acertou em {cont} tentativas.')
+
+# VERSÃO GUANABARA
+
+from random import randint
+computador = randint(0,10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais...')
+        elif jogador > computador:
+            print('Menos...')
+
+print('Acertou com {} tentativas. Parabéns!'.format(palpites))
